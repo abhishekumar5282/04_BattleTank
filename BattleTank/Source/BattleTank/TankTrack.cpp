ec93e592eba09db7c020e282f74d1d760201f7ff
abhibhai65 <abhishekkumar5282@gmail.com>
2019-03-21 13:58:23 +0530

    165 tank movememt

---
+++ BattleTank/Source/BattleTank/TankTrack.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+#include "BattleTank.h"
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s Throttles at %f"),*Name,Throttle);
+
+
+	auto ForceApplied = GetForwardVector()*Throttle*TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot =Cast<UPrimitiveComponent>( GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied,ForceLocation);
+}
